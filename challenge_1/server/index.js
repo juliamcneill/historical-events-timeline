16556281a49767008de0a9cba818ec19580831dc
Julia McNeill <juliammcneill@gmail.com>
2020-06-25 11:32:33 -0400

    Set up file structure and webpack

---
+++ challenge_1/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../client/dist"));
+
+module.exports.app = app;
+app.set("port", 3000);
+
+if (!module.parent) {
+  app.listen(app.get("port"));
+  console.log("Listening on", app.get("port"));
+}
