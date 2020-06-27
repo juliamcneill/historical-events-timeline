c572280616f2000f77b07557972dce0510f41795
Julia McNeill <juliammcneill@gmail.com>
2020-06-27 18:06:40 -0400

    Create mySQL database and insert events

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client/dist"));
 
@@ -13,3 +16,34 @@ if (!module.parent) {
   app.listen(app.get("port"));
   console.log("Listening on", app.get("port"));
 }
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "historical_events_timeline",
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected!");
+  }
+});
+
+var readAll = (callback) => {
+  connection.query("SELECT * FROM events", function (error, results) {
+    callback(error, results);
+  });
+};
+
+app.get("/events", (req, res) => {
+  readAll(function (error, results) {
+    if (error) {
+      res.status(404).json(error);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
